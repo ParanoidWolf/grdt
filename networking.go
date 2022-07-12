073d12f9c5f6e577f2639d7d3d9d3e9524bee20d
Paranoid Wolf <ajuph9224@gmail.com>
2022-07-12 21:25:37 +0530

    initial commit

--- networking.go
+++ networking.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"io/ioutil"
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
-    "crypto/tls"
+	"regexp"
 )
 
 func get_json(subreddit string) []byte {
+    // Setup http client with empty tls config for reddit url to work
 	client := &http.Client{
         Transport: &http.Transport{
             TLSClientConfig: &tls.Config{},
         },
     }
 
-    url := fmt.Sprintf("https://www.reddit.com/r/%s/top.json", subreddit)
+    url := fmt.Sprintf("https://www.reddit.com/r/%s/top.json?limit=100", subreddit)
     
     req, err := http.NewRequest("GET", url, nil)
     checkErr(err)
@@ -29,4 +32,29 @@ func get_json(subreddit string) []byte {
     checkErr(err)
 
     return body
+}
+
+func get_images(body []byte) []string {
+    var dat map[string]interface{}
+    var images_list []string
+    match, _ := regexp.Compile("^https?://.*/.*.(png|gif|webp|jpeg|jpg)$")
+
+    err := json.Unmarshal(body, &dat)
+    checkErr(err)
+
+    json_data := dat["data"].(map[string]interface{})
+    post_list := json_data["children"].([]interface{})
+    for i := 0; i < len(post_list); i++ {
+        post := post_list[i].(map[string]interface{})
+        post_data := post["data"].(map[string]interface{})
+        checkErr(err)
+        url := post_data["url_overridden_by_dest"]
+        if url != nil {
+             if match.MatchString(url.(string)) {
+                 images_list = append(images_list, url.(string))
+             }
+        }
+    }
+
+    return images_list
 }
